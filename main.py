8929fb0b18dc1cb57912638edc638f4d73f731e3
javad_fallah_official <javad.fallah.official@gmail.com>
2023-11-01 22:20:36 +0330

    extra things remoce error handeling added

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
-from googletrans import Translator
 import threading
-from PIL import Image
-import os
 import Telegram
 from Response import response
-from dotenv import load_dotenv
-
-# reading .env variables
-load_dotenv()
-
-translator = Translator()
-filename= ""
-comfyUrl =  os.getenv('COMFY_UI_URL') 
-outputPath = os.getenv('OUTPUT_PATH') 
-
-
 
 
 def main():
@@ -27,8 +13,8 @@ def main():
             for Message in data["result"]:
                 try:
                     threading.Thread(target=response, args=(Message,offset)).start()
-                except:
-                    pass
+                except Exception as e:
+                    print(f"ERROR: {e}")
                 #Basicly going to next message    
                 offset = data["result"][-1]["update_id"] + 1
         else:
